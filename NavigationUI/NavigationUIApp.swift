f42aebd7e83748472d1ba6b448fd9d45778a0c2a
Seth Faxon <seth.faxon@gmail.com>
2022-10-20 11:41:44 -0700

    Initial Commit

---
+++ NavigationUI/NavigationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationUIApp.swift
+//  NavigationUI
+//
+//  Created by Seth Faxon on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
