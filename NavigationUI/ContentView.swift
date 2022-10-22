1435ce8ff30d58d91e7d11d2b7b5613e09f03b00
Seth Faxon <seth.faxon@gmail.com>
2022-10-21 21:43:20 -0700

    Direction working for insert and remove

--- NavigationUI/ContentView.swift
+++ NavigationUI/ContentView.swift
@@ -136,6 +136,82 @@ extension View {
     }
 }
 
+struct InsertModifier: ViewModifier & Animatable {
+    init(offset: CGFloat, active: Bool, navigation: NavigationModel) {
+        self.offset = offset
+        self.active = active
+        progress = active ? 1 : 0
+        self.navigation = navigation
+    }
+
+    var active: Bool
+    var offset: CGFloat = .zero
+    var progress: CGFloat
+    var navigation: NavigationModel
+
+    var animatableData: CGFloat {
+        get { progress }
+        set { progress = newValue }
+    }
+
+    func body(content: Content) -> some View {
+        _ = print("InsertModifier progress: \(progress), active: \(active) ")
+        var progressPrime = progress
+        if !active {
+            progressPrime = 1 - progress
+        }
+        if navigation.isBack {
+            return content.offset(x: -offset * progress, y: 0)
+        } else {
+            if active {
+                return content.offset(x: offset * progressPrime, y: 0)
+            } else {
+                // new view coming in
+                print("InsertModifier offset: \(offset), progress: \(progress)")
+                return content.offset(x: offset * progress, y: 0)
+            }
+        }
+    }
+}
+
+struct RemoveModifier: ViewModifier & Animatable {
+    init(offset: CGFloat, active: Bool, navigation: NavigationModel) {
+        self.offset = offset
+        self.active = active
+        progress = active ? 1 : 0
+        self.navigation = navigation
+    }
+
+    var active: Bool
+    var offset: CGFloat = .zero
+    var progress: CGFloat
+    var navigation: NavigationModel
+
+    var animatableData: CGFloat {
+        get { progress }
+        set { progress = newValue }
+    }
+
+    func body(content: Content) -> some View {
+        _ = print("RemoveModifier progress: \(progress), active: \(active) ")
+        if navigation.isBack {
+            if active {
+                return content.offset(x: offset * progress, y: 0)
+            } else {
+                return content.offset(x: -offset * progress, y: 0)
+            }
+        } else {
+            if active {
+                return content.offset(x: -offset * progress, y: 0)
+            } else {
+                // new view coming in
+                print("RemoveModifier offset: \(offset), progress: \(progress)")
+                return content.offset(x: offset * progress, y: 0)
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     @StateObject var navigation = NavigationModel()
     @State var back: Bool = false
@@ -181,19 +257,25 @@ struct ContentView: View {
     }
 
     var t: AnyTransition {
-        print("current: \(navigation.current) isBack: \(navigation.isBack)")
-        if navigation.current == .landing && !navigation.isBack {
-            print("returning: .leading")
-            return AnyTransition.move(edge: .leading)
-        }
-        if navigation.isBack {
-            print("returning: .leading")
-            return AnyTransition.move(edge: .leading)
-//            return AnyTransition.modifier(active: ClipEffect(value: 0), identity: ClipEffect(value: 1))
-        }
-        print("returning: .trailing")
-//        return AnyTransition.opacity
-        return AnyTransition.move(edge: .trailing)
+        let insert = AnyTransition.modifier(active: InsertModifier(offset: 300, active: true, navigation: navigation),
+                                            identity: InsertModifier(offset: 300, active: false, navigation: navigation))
+        let remove = AnyTransition.modifier(active: RemoveModifier(offset: 150, active: true, navigation: navigation),
+                                            identity: RemoveModifier(offset: 150, active: false, navigation: navigation))
+
+        return AnyTransition.asymmetric(insertion: insert, removal: remove)
+//        print("current: \(navigation.current) isBack: \(navigation.isBack)")
+//        if navigation.current == .landing && !navigation.isBack {
+//            print("returning: .leading")
+//            return AnyTransition.move(edge: .leading)
+//        }
+//        if navigation.isBack {
+//            print("returning: .leading")
+//            return AnyTransition.move(edge: .leading)
+        ////            return AnyTransition.modifier(active: ClipEffect(value: 0), identity: ClipEffect(value: 1))
+//        }
+//        print("returning: .trailing")
+        ////        return AnyTransition.opacity
+//        return AnyTransition.move(edge: .trailing)
     }
 
     @ViewBuilder
